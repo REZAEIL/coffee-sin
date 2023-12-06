9c6b5b94923bc950290bcecf40b6bf95f730652f
REZAEIL <150054245+REZAEIL@users.noreply.github.com>
2023-12-06 15:52:16 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById("phone-button").addEventListener("click", function() {
+  var phoneNumber = "09334427595"; // اینجا شماره تلفنت رو قرار بده
+  window.location.href = "tel:" + phoneNumber;
+});
